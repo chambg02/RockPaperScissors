c1fefba1cc0fd0d052f79e756f5dc41c8378831d
chambg02 <chambg02@gmail.com>
2022-04-24 20:46:54 +0800

    Add prompt user input

--- game.js
+++ game.js
@@ -1,11 +1,9 @@
-console.log('hello world');
 
 
+// Function to make the computer pick Rock, Paper or Scissors randomly:
 function computerPlay() {
     randomNumber = Math.floor(Math.random()*3+1);
     let computerChoice = '';
-
-
     if (randomNumber == 1) {
         computerChoice = "rock";
     } else if (randomNumber == 2) {
@@ -14,5 +12,34 @@ function computerPlay() {
         computerChoice = "scissors";
     }
     return computerChoice;
+}
+
+// Function to prompt user input and convert it for compatibility:
+function playerPlay() {
+    let playerInput ='';
+    let playerChoice = '';
+    
+    playerInput = prompt("Choose Rock, Paper or Scissors!")
+    playerChoice = playerInput.toLowerCase();
+
+    return playerChoice;
+}
+
+// Function to prompt user input, make computer generate pick then determine a winner:
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+     playerSelection = playerPlay();
+    console.log("Player chose " + playerSelection);
+    console.log("Computer chose " + computerSelection);
+
+
+
 
 }
+
+
+
+
+
+
+
