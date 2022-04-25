70edc2a27b69b79f29e8c5101aac9a00d2540a41
chambg02 <chambg02@gmail.com>
2022-04-25 14:38:43 +0800

    Update code to work for 1 round.

--- game.js
+++ game.js
@@ -27,14 +27,43 @@ function playerPlay() {
 
 // Function to prompt user input, make computer generate pick then determine a winner:
 function playRound(playerSelection, computerSelection) {
+    let result = '';
     computerSelection = computerPlay();
-     playerSelection = playerPlay();
+    playerSelection = playerPlay();
     console.log("Player chose " + playerSelection);
     console.log("Computer chose " + computerSelection);
 
+    // Results for Player selecting Rock
+    if (playerSelection == 'rock' && computerSelection == 'rock') {
+        result = 'Draw!';
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        result = 'Paper beats rock. You Lose!';
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        result = 'Rock beats scissors. You Win!'
+    }
+    
+    // Results for Player selecting Paper
+    if (playerSelection == 'paper' && computerSelection == 'paper') {
+        result = 'Draw!';
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        result = 'Scissors beat paper. You Lose!';
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        result = 'Paper beats rock. You Win!'
+    }
+    
+    // Results for Player selecting Scissors
+    if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+        result = 'Draw!';
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        result = 'Rock beats scissors. You Lose!';
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        result = 'Scissors beat paper. You Win!'
+    }
 
-
-
+    return result;
 }
 
 
